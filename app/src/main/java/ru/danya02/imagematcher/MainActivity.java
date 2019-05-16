cdda8312b7ccadb7a7e51eac59e2f633adc095d5
Danya Generalov <danya@danya02.ru>
2019-05-16 11:29:45 +0300

    Adding main activity layout.

--- app/src/main/java/ru/danya02/imagematcher/MainActivity.java
+++ app/src/main/java/ru/danya02/imagematcher/MainActivity.java
@@ -1,7 +1,15 @@
 package ru.danya02.imagematcher;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.DataSetObserver;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,86 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        emptyLabel = findViewById(R.id.empty_list_label);
+        recyclerView = findViewById(R.id.rv_image_view);
+
+        FloatingActionButton fab = findViewById(R.id.fab_add_to_index);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
+    private View emptyLabel;
+
+    private RecyclerView recyclerView;
+    private Adapter adapter = new Adapter() {
+        @Override
+        public void registerDataSetObserver(DataSetObserver observer) {
+
+        }
+
+        @Override
+        public void unregisterDataSetObserver(DataSetObserver observer) {
+
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return 0;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+    };
+
+
+    void updateObjectVisibility(){
+        if (adapter.isEmpty()) {
+            recyclerView.setVisibility(View.GONE);
+            emptyLabel.setVisibility(View.VISIBLE);
+        } else {
+            recyclerView.setVisibility(View.VISIBLE);
+            emptyLabel.setVisibility(View.GONE);
+        }
+    }
+
 }
